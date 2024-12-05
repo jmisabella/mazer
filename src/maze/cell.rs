5e2dfca86317aec3aef90815ce31601e7808d130
Jeffrey <jeff.isabella@gmail.com>
2024-12-04 20:46:11 -0500

    cell struct without any functionality
    cell struct without any functionality

---
+++ src/maze/cell.rs
@@ -0,0 +1,60 @@
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug, Clone, Eq, Hash, PartialEq)] // Traits similar to Scala's case class behavior
+pub struct Coordinates {
+    pub x: u32,
+    pub y: u32
+}
+impl Default for Coordinates {
+    fn default() -> Self {
+        Self {
+            x: 0,
+            y: 0            
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)] // Traits similar to Scala's case class behavior
+pub enum MazeType {
+    Orthogonal,
+    // Sigma,
+    // Delta,
+    // Polar
+}
+
+#[derive(Debug, Clone, PartialEq)] // Traits similar to Scala's case class behavior
+pub enum CellOrientation {
+    Normal,
+    // Inverted
+}
+
+#[derive(Debug, Clone, PartialEq)] // Traits similar to Scala's case class behavior
+pub struct Cell {
+    pub coords: Coordinates,
+    pub maze_type: MazeType,
+    pub neighbors_by_direction: HashMap<String, Coordinates>, // Map[String, Coordinates]
+    pub linked: HashSet<Coordinates>,                        // Set[Coordinates]
+    pub distance: i32,                                       // Int
+    pub is_start: bool,                                      // Boolean
+    pub is_goal: bool,                                       // Boolean
+    pub on_solution_path: bool,                              // Boolean
+    pub orientation: CellOrientation,                        // CellOrientation (enum)
+    pub visited: bool,                                       // Boolean
+}
+
+impl Default for Cell {
+    fn default() -> Self {
+        Self {
+            coords: Coordinates::default(),
+            maze_type: MazeType::Orthogonal,
+            neighbors_by_direction: HashMap::new(),
+            linked: HashSet::new(),
+            distance: 0,
+            is_start: false,
+            is_goal: false,
+            on_solution_path: false,
+            orientation: CellOrientation::Normal, // Assuming CellOrientation has a Normal variant
+            visited: false,
+        }
+    }
+}
