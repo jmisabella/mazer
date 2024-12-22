cc86e7c1164902038ae81ef8e6b6efc595f7e601
Jeffrey <jeff.isabella@gmail.com>
2024-12-21 21:00:04 -0500

    added generate function to root mod, accepts a maze-request JSON string
    added generate function to root mod, accepts a maze-request JSON string

--- src/maze/algorithms/mod.rs
+++ src/maze/algorithms/mod.rs
@@ -37,3 +37,26 @@ impl MazeAlgorithm {
         return grid.clone();
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::maze::generate;
+
+    #[test]
+    fn test_recursive_backtracker_orthogonal_12_x_12_maze_generation_from_json() {
+        let json = r#"
+        {
+            "maze_type": "Orthogonal",
+            "width": 12,
+            "height": 12,
+            "algorithm": "RecursiveBacktracker",
+            "start": { "x": 0, "y": 0 },
+            "goal": { "x": 11, "y": 11 }
+        }
+        "#;
+        let maze = generate(json);
+        assert!(maze.is_perfect_maze());
+        println!("\n\nRecursive Backtracker\n\n{}\n\n", maze.to_asci());
+
+    }
+}
